c13860ecf9e5018191a3ec1479d9f49c4615d0c9
theabroadwolf <belalgamal00@gmail.com>
2023-09-28 04:33:16 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import SideNavbar from './components/SideNavbar/SideNavbar';
+
+function App() {
+  return (
+    <>
+      <SideNavbar />
+    </>
+  );
+}
+
+export default App;
